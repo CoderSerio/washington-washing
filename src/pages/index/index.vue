2f3ba502bf4a43fb5c7e946ae9ee3cdbff6b5187
CoderSerio <2779066456@qq.com>
2024-04-20 03:36:35 +0800

    shit: 经过一些考虑，决定把node_modules一起带进来

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,58 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+  <ant-container title="按钮类型" className="list">
+    <ant-button type="primary" bind:tap="handleTap"> 主要按钮 </ant-button>
+
+    <ant-button type="default" bind:tap="handleTap"> 次要按钮 </ant-button>
+
+    <ant-button type="text" bind:tap="handleTap"> 文本按钮 </ant-button>
+  </ant-container>
+
+  <ant-container title="带辅助文案按钮" className="list">
+    <ant-button type="primary" subText="副标题" bind:tap="handleTap">
+      主要按钮
+    </ant-button>
+
+    <ant-button type="default" subText="副标题" bind:tap="handleTap">
+      次要按钮
+    </ant-button>
+
+    <ant-button type="text" subText="副标题" bind:tap="handleTap">
+      文本按钮
+    </ant-button>
+  </ant-container>
+
+  <ant-container title="禁用" className="list">
+    <ant-button type="primary" disabled> 主要按钮 </ant-button>
+
+    <ant-button type="default" disabled> 次要按钮 </ant-button>
+
+    <ant-button type="text" disabled> 文本按钮 </ant-button>
+  </ant-container>
+
+  <ant-container title="加载状态" className="list">
+    <ant-button type="primary" loading> 主要按钮 </ant-button>
+
+    <ant-button type="default" loading> 次要按钮 </ant-button>
+
+    <ant-button type="text" loading> 文本 </ant-button>
+  </ant-container>
+
+  <ant-container title="危险" className="list">
+    <ant-button type="primary" danger bind:tap="handleTap">
+      主要按钮
+    </ant-button>
+
+    <ant-button type="default" danger bind:tap="handleTap">
+      次要按钮
+    </ant-button>
+
+    <ant-button type="text" danger bind:tap="handleTap"> 文本按钮 </ant-button>
+  </ant-container>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+import { ref } from "vue";
+const title = ref("Hello");
 </script>
 
 <style>
